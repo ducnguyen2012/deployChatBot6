7363be36ce1d6eae5aeb7a4faa2e1e7f22d921d1
ducnguyen2012 <166124443+ducnguyen2012@users.noreply.github.com>
2025-03-13 13:27:21 +0700

    Update MCQGen.py

--- flaskWebsite/MCQModule/MCQGen.py
+++ flaskWebsite/MCQModule/MCQGen.py
@@ -39,7 +39,7 @@ def MCQResponse(num_questions, difficulty):
     prompt = difficulty_prompt.get(difficulty, "Invalid difficulty level. Please choose from 'easy', 'medium', or 'hard'.")
 
     # Initialize GenerativeModel
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-1.5-pro')
 
     # Generate content (MCQ questions)
     response = model.generate_content(prompt)
