9eb8fe2680f9c2f31bc58485d5a7c3f0555e357d
ducnguyen2012 <166124443+ducnguyen2012@users.noreply.github.com>
2025-03-13 15:19:15 +0700

    Update textSplit.py

--- flaskWebsite/MCQModule/textSplit.py
+++ flaskWebsite/MCQModule/textSplit.py
@@ -8,26 +8,26 @@ def extract_text_from_pdf(pdf_path):
         num_pages = len(pdf_reader.pages)
         for page_num in range(num_pages):
             page = pdf_reader.pages[page_num]
-            # Extract text and split into lines
+            #! extract text và split lines
             page_text = page.extract_text().splitlines()
-            # Iterate through lines, keeping only those starting with a letter
+            # for qua từng dòng để lấy text
             text += "\n".join([line for line in page_text if line[0].isalpha()])
     return text
 def select_text_from_pdf(pdf_path, batch_size=10000, num_batches=2):
-    # Extract text from PDF
+    
 
     pdf_text = extract_text_from_pdf(pdf_path)
 
-    # Calculate the total number of words in the preprocessed text
+    
     total_words = len(pdf_text)
 
-    # Randomly select starting indices for the batches
+    # random chọn start indices
     selected_start_indices = random.sample(range(total_words - (batch_size * num_batches)), num_batches)
 
-    # Initialize the selected text
+    
     selected_text = ""
 
-    # Extract text for each selected batch and concatenate
+    # extract text trong từng patch và concat
     for start_index in selected_start_indices:
         end_index = start_index + batch_size
         selected_text += pdf_text[start_index:end_index]
